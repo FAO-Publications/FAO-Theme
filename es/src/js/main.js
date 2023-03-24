93bd9cea04ac5e3db383dd9e15f6d8f9f1c00003
FAO-Publications <paolo.todini@fao.org>
2023-03-24 18:36:46 +0100

    test spanish

---
+++ es/src/js/main.js
@@ -0,0 +1,188 @@
+import pub from '../../../publication.js'
+
+$(window).on("load", function () {
+    window.localStorage.clear();
+    var sPageURL = window.location.href;
+
+    for (var i = 0; i < pub.languages.length; i++) {
+
+        var found = sPageURL.indexOf(pub.languages[i].code) > 0;
+        if (found) {
+
+            var context = sPageURL.split(pub.languages[i].code)[0];
+
+            for (var j = 0; j < pub.languages[i].menu.length; j++) {
+
+                var title = pub.languages[i].menu[j].title;
+                var page = pub.languages[i].menu[j].url;
+                var url = context + pub.languages[i].code + "/" + pub.languages[i].acronym + "-" + pub.languages[i].year + "/" + page + ".html";
+              
+
+                if (window.localStorage.getItem(page) == null) {
+                    downloadPage(page, url, title, pub.languages[i].menu);
+                }
+            }
+        }
+    }
+});
+
+function downloadPage(page, url, title, menu) {
+
+    $.ajax({
+        url: url,
+        success: function (data) {
+
+            var body = /<main.*>([\s\S]+)<\/main>/.exec(data);
+
+            var search = {};
+            search["index"] = "";
+            for (let i = 0; i < menu.length; i++) {
+                search[menu[i].url] = "";
+            }
+
+            window.localStorage.setItem(page, JSON.stringify({ "title": title, "url": url, "body": body, "search": search }));
+        }
+    });
+}
+
+
+// Cache selectors
+var lastId, topMenu, topMenuHeight, menuItems, scrollItems;
+
+
+document.querySelector('.third-button')?.addEventListener('click', function () {
+
+    document.querySelector('.menu-icon').classList.toggle('open');
+});
+
+$(document).ready(function () {
+
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+    // Se la larghezza è inferiore a 576px, aggiungi la classe "mobile" all'elemento con ID "elementId"
+    if (width < 576) {
+        var element = document.getElementById("searchMobile");
+        element.classList.add("dropdown-menu");
+    }
+
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+    // Reg Exp to format title if any HTML tag is present
+    var theTitle = document.title.replace(/(<([^>]+)>)/gi, " ");
+    document.title = theTitle;
+
+
+
+    // Set active link from menu
+    var location = window.location.pathname.split('/').pop();
+
+    $('ul.nav li a.nav-link').each(function () {
+        if (this.href.split('/').pop() === location) {
+            $(this).addClass('active');
+            if ($(this).parent('li').parent('ul').parent('li').find('span.submenu').length) {
+            } else {
+                $(this).parent('li').css('border-bottom', 'none');
+            }
+        }
+    });
+
+
+    $('li.active').unbind().click(function () {
+        $(this).parent("ul").find("li").not(".active").toggle();
+        $(this).parent("ul").toggleClass('grey');
+    });
+
+
+
+    let mybutton = document.getElementById("btn-back-to-top");
+    mybutton?.addEventListener("click", backToTop);
+
+    function backToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+});
+
+
+// Cache selectors
+topMenu = $("#top-menu");
+topMenuHeight = topMenu.outerHeight() + 15;
+
+// All list items
+menuItems = topMenu.find("a");
+
+// Anchors corresponding to menu items
+scrollItems = menuItems.map(function () {
+    var item = $(this).attr("href");
+    if (item != '#') { return $(item) }
+});
+
+
+// Bind to scroll
+$(window).on('scroll', function () {
+
+   
+    // Get container scroll position
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+
+    // Get id of current scroll item
+    var cur = $(scrollItems).map(function () {
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+
+
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    lastId = lastId ? lastId : "";
+
+    for (var i = 0; i < menuItems.length; i++) {
+        $(menuItems[i]).removeClass("active");
+
+        if (id !== "") {
+            if ($(menuItems[i]).attr("href") === "#" + id) {
+ 
+                $(menuItems[i]).addClass("active");
+            }
+        }
+
+
+    }
+});
+
+// Select all the links with the class "close-navbar".
+
+const closeNavbarLinks = document.querySelectorAll('.close-navbar');
+
+// Add a click event handler to each link.
+
+closeNavbarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        // Close the navbar if it's open.
+        const navbarCollapse = document.getElementById('navbarCollapse');
+        const menuIcon = document.querySelector('.menu-icon');
+        if (navbarCollapse.classList.contains('show')) {
+            navbarCollapse.classList.remove('show');
+            menuIcon.classList.remove('open');
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    // Get the current URL.
+    var url = window.location.href;
+
+    // Check if the URL ends with "?search".
+    if (url.indexOf('?search') !== -1) {
+        // Add the class "show" to the div with id="searchMobile".
+        $('#searchMobile').addClass('show');
+    }
+});
+
+
+
+
--- publication.js
+++ publication.js
@@ -167,6 +167,172 @@ module.exports = {
                 
             ],
         },
+        {
+            lang: "es",
+            langText: "Spanish",
+            code: "job-number-es",
+            titlePubblications: "RURAL",
+            acronym: 'chapters',
+            year: '2023',
+            metaTitle: "RURAL POVERTY ANALYSIS",
+            metaDescription: "A technical guide for measuring, profiling and targeting in programmes and projects",
+            DOI: "10.4060/job-number-es",
+            pubTitle: "RURAL<br /> POVERTY<br /> ANALYSIS",
+            pubSubTitle: "A technical guide for measuring, profiling and targeting in programmes and projects",
+            images: "primary-image",
+            menu: [
+                {
+                    pubTitle: "RURAL<br /> POVERTY<br /> ANALYSIS",
+                    title: "Front Matter",
+                    url: "front-matter",
+                    img: "",
+                    isChapter: false,
+                    subMenu: [{
+                        subMenuTitle: "FOREWORD",
+                        subMenuLink: "#item-1-1"
+                    },
+                    {
+                        subMenuTitle: "PREFACE",
+                        subMenuLink: "#item-1-2"
+                    },
+                    {
+                        subMenuTitle: "ACKNOWLEDGEMENTS",
+                        subMenuLink: "#item-1-3"
+                    },
+                    {
+                        subMenuTitle: "ABBREVIATIONS AND ACRONYMS",
+                        subMenuLink: "#item-1-4"
+                    },
+                    {
+                        subMenuTitle: "EXECUTIVE SUMMARY",
+                        subMenuLink: "#item-1-5"
+                    },
+                    ],
+                },
+
+                {
+                    pubTitle: "RURAL<br /> POVERTY<br /> ANALYSIS",
+                    title: "MEASURING RURAL POVERTY",
+                    url: "chapter-1",
+                    img: "chapter-image",
+                    isChapter: true,
+                    chapterNumber: "1",
+                    subMenu: [{
+                        subMenuTitle: "1.1. Introduction",
+                        subMenuLink: "#item-1-1"
+                    },
+                    {
+                        subMenuTitle: "1.2. Poverty measurement",
+                        subMenuLink: "#item-1-2"
+                    },
+                    {
+                        subMenuTitle: "1.3. Measuring monetary poverty",
+                        subMenuLink: "#item-1-3"
+                    },
+                    {
+                        subMenuTitle: "1.4. Measuring multidimensional poverty",
+                        subMenuLink: "#item-1-4"
+                    },
+                    ],
+                },
+                {
+                    pubTitle: "RURAL<br /> POVERTY<br /> ANALYSIS",
+                    title: "CHARACTERIZING THE RURAL POOR AND EXTREME POOR",
+                    url: "chapter-2",
+                    img: "chapter-image",
+                    isChapter: true,
+                    chapterNumber: "2",
+                    subMenu: [{
+                        subMenuTitle: "2.1.Introduction",
+                        subMenuLink: "#item-1-1"
+                    },
+                    {
+                        subMenuTitle: "2.2 Poverty profiles: going beyond counting the rural poor and extreme poor",
+                        subMenuLink: "#item-1-2"
+                    },
+                    {
+                        subMenuTitle: "2.3. Poverty maps",
+                        subMenuLink: "#item-1-3"
+                    },
+                    ],
+                },
+                {
+                    pubTitle: "RURAL<br /> POVERTY<br /> ANALYSIS",
+                    title: "TARGETING FOR RURAL POVERTY REDUCTION INTERVENTIONS",
+                    url: "chapter-3",
+                    img: "chapter-image",
+                    isChapter: true,
+                    chapterNumber: "3",
+                    subMenu: [{
+                        subMenuTitle: "3.1.Introduction",
+                        subMenuLink: "#item-1-1"
+                    },
+                    {
+                        subMenuTitle: "3.2. Current debates on targeting",
+                        subMenuLink: "#item-1-2"
+                    },
+                    {
+                        subMenuTitle: "3.3. Targeting mechanisms",
+                        subMenuLink: "#item-1-3"
+                    },
+                    {
+                        subMenuTitle: "3.4. The targeting process",
+                        subMenuLink: "#item-1-4"
+                    },
+                    ],
+                },
+                {
+                    pubTitle: "RURAL<br /> POVERTY<br /> ANALYSIS",
+                    title: "WHAT’S NEXT?",
+                    url: "chapter-4",
+                    img: "chapter-image",
+                    isChapter: true,
+                    chapterNumber: "4",
+                    subMenu: [
+                    ],
+                },
+                {
+                    pubTitle: "RURAL<br /> POVERTY<br /> ANALYSIS",
+                    title: "REFERENCES",
+                    url: "references",
+                    img: "",
+                    isChapter: false,
+                    subMenu: [
+
+                    ],
+                },
+                {
+                    pubTitle: "RURAL<br /> POVERTY<br /> ANALYSIS",
+                    title: "ANNEXES",
+                    url: "annexes",
+                    img: "",
+                    isChapter: false,
+                    subMenu: [{
+                        subMenuTitle: "Annex 1",
+                        subMenuLink: "#item-1-1"
+                    },
+                    {
+                        subMenuTitle: "Annex 2",
+                        subMenuLink: "#item-1-2"
+                    },
+                    {
+                        subMenuTitle: "Annex 3",
+                        subMenuLink: "#item-1-3"
+                    },
+                    {
+                        subMenuTitle: "Annex 4",
+                        subMenuLink: "#item-1-4"
+                    },
+                    {
+                        subMenuTitle: "Annex 5",
+                        subMenuLink: "#item-1-5"
+                    },
+                    ],
+                },
+
+
+            ],
+        },
 
     ]
 
